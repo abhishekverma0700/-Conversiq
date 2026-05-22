037d01c64525082319ea44d2f85f0d0f61296575
abhishekverma0700 <abhishekverma0700@gmail.com>
2026-05-22 18:20:03 +0530

    Fixed Python runtime and gunicorn configuration

--- backend/render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-pip install --upgrade pip==24.0
-pip install setuptools wheel
-pip install gunicorn
-pip install tiktoken==0.4.0 --prefer-binary
-
-pip install -r requirements.txt
